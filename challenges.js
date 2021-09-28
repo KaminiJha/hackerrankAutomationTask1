b8d8b50ad9bd2b37fac4d1b6bff6b43dc76136fe
KaminiJha <kamini123skt@gmail.com>
2021-09-28 16:14:19 +0530

    completed create challeneg

--- challenges.js
+++ challenges.js
@@ -1,86 +1,102 @@
-const { TIMEOUT } = require('dns');
-const puppy=require('puppeteer');
-async function openBrowser()
-{
-    const browser=await puppy.launch({
-        headless:false,
-        defaultViewport:false,
-        args:[
-            '--start-maximized'
-        ],
-    });
-    // browser.pages().then(function(tabs)
-    // {
-    //     tabs[0].goto("https://www.google.com")
-    // })
-    // for(let i=0;i<10;i++)
-    // {
-    //     //here we have not used await toh koi time waste nhi hoga u can doevertything parallely
-    //     browser.newPage().then(function(tab)
-    //     {
-    //         tab.goto("https://www.google.com")
-    //     })
-    //     // (await (await browser).newPage()).goto("https://www.google.com");
+const puppy = require("puppeteer");
 
-    // }
-    const tabs=await browser.pages();
-    const tab=tabs[0]
-    await tab.goto("https://www.hackerrank.com/auth/login");
-    //$->quryselector
-    //$$->querySelectorAll
-    //-----login steps
-    let username=await tab.$("#input-1");
-    let password=await tab.$("#input-2");
-    let remMe=await tab.$(".checkbox-input");
-    let loginBtn=await tab.$('[data-analytics="LoginPassword"]')
-    await username.type("kamini123skt@gmail.com");
-    await password.type("kamini@1")
-    await remMe.click()
-    await loginBtn.click()
+let moderators = ["bansalbhavesh47", "bansalbhavesh50"];
 
-    await tab.waitForNavigation()
-    await tab.waitForTimeout(2000)
+async function openBrowser(url) {
+  const browser = await puppy.launch({
+    headless: false,
+    defaultViewport: false,
+    args: [
+      "--start-maximized", // you can also use '--start-fullscreen'
+    ]
+  });
+  // browser.pages().then(function(tabs) {
+  //     tabs[0].goto("https://www.google.com");
+  // })
+  // for(let i = 0; i < 9; i++) {
+  //     browser.newPage().then(function(tab) {
+  //         tab.goto("https://www.google.com");
+  //     })
+  // }
 
-    //click on user icon   
-    await tab.click('div[data-analytics="NavBarProfileDropDown"]')
-    await tab.waitForTimeout(2000)
-    await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]')
+  const tabs = await browser.pages();
+  const tab = tabs[0];
+  await tab.goto("https://www.hackerrank.com/auth/login");
+  //filing login details
+  let usernameInputTab = await tab.$("#input-1");
+  let passwordInputTab = await tab.$("#input-2");
+  let rememberCheckbox = await tab.$(".checkbox-input");
+  let loginButton = await tab.$('[data-analytics="LoginPassword"]');
+  await usernameInputTab.type("kamini123skt@gmail.com");
+  await passwordInputTab.type("kamini@1");
+  await rememberCheckbox.click();
+  await loginButton.click();
+  //navigating to other window
+  await tab.waitForNavigation({ waitUntil: "networkidle2" });
+  //click dropdown and select administration
+  await tab.waitForSelector('[data-analytics="NavBarProfileDropDown"]', {
+    visible: true,
+  });
+  let myProfileButton = await tab.$('[data-analytics="NavBarProfileDropDown"]');
+  await myProfileButton.click();
+  let administrationButton = await tab.$(
+    '[data-analytics="NavBarProfileDropDownAdministration"]'
+  );
+  await administrationButton.click();
 
-    // await tab.waitForNavigation()
-    await tab.waitForTimeout(3000)
-
-    //opening administration screen
-    let challenges=await tab.$("#content>div>div>div>section>header>ul>li:nth-child(2)>a")
-   await challenges.click();
-   await tab.waitForTimeout(2000)
-
-   //selecting frst challenge and clicking on it
-   let selectChallenge1=await tab.$('#content>div>div>div>section>div.table-wrap.mlT>div>a')
-   await selectChallenge1.click()
-   await tab.waitForTimeout(2000);
-
-   //selecting moderators
-   let selectModerators=await tab.$('#content>div>section>header>div>div.tabs-cta-wrapper>ul>li:nth-child(2)')
-   await selectModerators.click()
-   console.log("moderators reached!!!")
-
-
-
-
-
-
-
-
-    // await tab.waitForNavigation()
-    // console.log("navigated")
-    // console.log("wait kra and selected")
-    // await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]')
-    // console.log("click kra")
-    // await tab.waitForNavigation()
-    // await tab.click("a[href='/administration/challenges']")
-    // await tab.waitForNavigation()
-    // await tab.click('a')
-    
-    
+  await tab.waitForNavigation({waitUntil:"networkidle2"})
+  //manage challenge click
+  await tab.waitForSelector(".admin-tabbed-nav a");
+  let administrationTabs = await tab.$$(".admin-tabbed-nav a");
+  await administrationTabs[1].click();
+  //click create and create challnge
+  await tab.waitForSelector(".btn.btn-green.backbone.pull-right", {visible: true});
+  let createChallengeButton = await tab.$(".btn.btn-green.backbone.pull-right");
+  await createChallengeButton.click();
+  await tab.waitForSelector("#input_format-container .CodeMirror-code");
+  let challengeNameInput = await tab.$("#name");
+  let challengeDescriptionInput = await tab.$("#preview");
+  await challengeNameInput.type("kjdsbf");
+  await challengeDescriptionInput.type("kjdsbf");
+  await tab.waitForSelector(".CodeMirror-code");
+  let codeTextAreas = await tab.$$(".CodeMirror-code");
+await tab.evaluate( () => {
+    window.scrollBy(0, window.innerHeight);
+});
+for(let i in codeTextAreas) {
+        await codeTextAreas[i].click();
+        await codeTextAreas[i].type("kjdsbf");
 }
-openBrowser()
+await codeTextAreas[1].click();
+await codeTextAreas[1].type("jbcbbwc");
+//adding tags
+await tab.waitForSelector("#tags_tagsinput");
+let tagsTextArea = await tab.$("#tags_tagsinput");
+await tagsTextArea.click();
+await tagsTextArea.type("kjdsbf");
+await tab.keyboard.press("Enter");
+//save challenge
+let saveChangesButton = await tab.$(".save-challenge.btn.btn-green");
+await saveChangesButton.click();
+//adding moderators
+await tab.waitForSelector('[data-tab="moderators"]');
+let moderatorButton = await tab.$('[data-tab="moderators"]');
+await moderatorButton.click();
+
+await tab.waitForSelector("#moderator")
+let moderatorTextArea = await tab.$("#moderator");
+for(let moderator of moderators) {
+    await moderatorTextArea.type(moderator);
+    await tab.keyboard.press("Enter");
+}
+//scroll 
+await tab.evaluate( () => {
+  window.scrollTo(0,window.document.body.scrollHeight);
+});
+//saving challenge
+await tab.waitForSelector(".save-challenge.btn.btn-green");
+let saveButton = await tab.$(".save-challenge.btn.btn-green");
+console.log("clci")
+await saveButton.click();
+}
+openBrowser("https://www.google.com");
